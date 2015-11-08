2ee97c9f7c98ff0b37922a260e6144417af9a9e8
omri klein <omrik2007@gmail.com>
2015-11-08 17:18:19 +0200

    שם
    עמרי ועמית

---
+++ ReversiArtificialIntelligence/AwesomePlayer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversiArtificialIntelligence
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AwesomePlayer : IReversiPlayer
+    {
+        public Point NextMove(Disc[,] board, Disc playerColor)
+        {
+            Point bestMove = new Point(0, 0);
+            int MaxD = 1;
+            
+            foreach (Point p in ReversiGame.ValidMoves(board, playerColor))
+            {
+                int score = ReversiGame.Score(ReversiGame.PlayTurn(board, p, playerColor), playerColor);
+                if (board.At(p) == playerColor)
+                {
+
+                }
+
+                if (score > MaxD)
+                {
+                    bestMove = p;
+                    MaxD = score;
+                }
+            }
+            return bestMove;
+        }
+    }
+}
