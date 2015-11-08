b57287fd754ad64e44a8981107ccecfb2dce0289
orenjp <orenjp@gmail.com>
2015-11-08 17:47:13 +0200

    ggaseioqwj
    qkwjeqweqw

--- ReversiArtificialIntelligence/Goyuojp.cs
+++ ReversiArtificialIntelligence/Goyuojp.cs
@@ -15,13 +15,44 @@ namespace ReversiArtificialIntelligence
         {
             Point bestMove = new Point(0, 0);
             int bestScore = int.MinValue;
+            bool flag = false, flag2=false;
             foreach (Point p in ReversiGame.ValidMoves(board, playerColor))
             {
-                int score = ReversiGame.Score(ReversiGame.PlayTurn(board, p, playerColor), playerColor);
-                if (score > bestScore)
+                if ((p.X == 0 && p.Y == 0) || (p.X == 0 && p.Y == 7) || (p.Y == 0 && p.X == 7) || (p.X == 7 && p.Y == 7)) return p;
+                else
                 {
+                    if ((p.Y == 0) || (p.Y == 7) || (p.X == 0) || (p.X == 7))
+                    {
+                        return p;
+                    }
+                   
+                }
+                int score = ReversiGame.Score(ReversiGame.PlayTurn(board, p, playerColor), playerColor);
+                if ((score > bestScore)&&((p.X!=1)&&(p.Y!=1)&&(p.Y!=6)&&(p.X!=6)))
+
+                {
+                    flag=true;
                     bestMove = p;
                     bestScore = score;
+                }
+                else
+                {
+                }
+            }
+            if(flag==false)
+            {
+                foreach (Point p in ReversiGame.ValidMoves(board, playerColor))
+                {
+                    if ((p.X != 1 && p.Y != 1)&&(p.X != 1 && p.Y != 6)&&(p.X != 6 && p.Y != 1)&&(p.X != 6 && p.Y != 6))
+                    {
+                        flag2 = true;
+                        return p;
+                    }
+                   
+                }
+                if (flag2 == false)
+                {
+                    bestMove = ReversiGame.ValidMoves(board, playerColor).First();
                 }
             }
             return bestMove;
