90dee60933faf4e4ad5bd8e54b3aa73d16566cf9
unknown <USER1@handas16.DOMAIN.local>
2015-11-08 17:17:18 +0200

    Initial Commit

---
+++ ReversiArtificialIntelligence/MinimaxPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReversiArtificialIntelligence
+{
+    class MinimaxPlayer : IReversiPlayer
+    {
+        public Point NextMove(Disc[,] board, Disc playerColor)
+        {
+            Point bestMove = new Point(0, 0);
+            int bestScore = int.MinValue;
+            foreach (Point p1 in ReversiGame.ValidMoves(board, playerColor))
+            {
+                ReversiGame.PlayTurn(board, p1, playerColor);
+                foreach (Point p2 in ReversiGame.ValidMoves(board, playerColor))
+                {
+                    int score = ReversiGame.Score(board, playerColor);
+                    if (score > bestScore)
+                    {
+                        bestMove = p1;
+                        bestScore = score;
+                    }
+                }
+            }
+            return bestMove;
+        }
+    }
+}
