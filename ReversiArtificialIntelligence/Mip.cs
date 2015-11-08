693a35d029741919bcbfc4ec87fef52b2170c5ef
denisgur <denisgurarie@gmail.com>
2015-11-08 17:46:22 +0200

    Mip2
    Demis and Yoav

--- ReversiArtificialIntelligence/Mip.cs
+++ ReversiArtificialIntelligence/Mip.cs
@@ -15,69 +15,107 @@ namespace ReversiArtificialIntelligence
         {
             Point bestMove= new Point(0,0);
             int bestScore = int.MinValue;
-            foreach (Point p in ReversiGame.ValidMoves(board, playerColor))
+            Point DRCorner = new Point(7, 7);
+            bool isDRfull =false;
+            Point URCorner = new Point(7, 0);
+            bool isURfull =false;
+            Point DLCorner = new Point(0, 7);
+            bool isDLfull =false;
+            Point ULCorner = new Point(0, 0);
+            bool isULfull =false;
+            bool shouldDoDumm = true;
+
+            if (ReversiGame.IsValidMove(board,DRCorner,playerColor))
             {
-                int score = ReversiGame.Score(ReversiGame.PlayTurn(board, p, playerColor), playerColor);
-                if(score > bestScore)
+                bestMove = DRCorner;
+                shouldDoDumm = false;
+                isDRfull = true;
+            }
+            else if (ReversiGame.IsValidMove(board, URCorner, playerColor))
+            {
+                bestMove = URCorner;
+                shouldDoDumm = false;
+                isURfull = true;
+            }
+            else if (ReversiGame.IsValidMove(board, DLCorner, playerColor))
+            {
+                bestMove = DLCorner;
+                shouldDoDumm = false;
+                isDLfull = true;
+            }
+            else if (ReversiGame.IsValidMove(board, ULCorner, playerColor))
+            {
+                bestMove = ULCorner;
+                shouldDoDumm = false;
+                isULfull = true;
+            }
+                if(shouldDoDumm)
                 {
-                    bestMove = p;
-                    bestScore = score;
+                    foreach (Point p in ReversiGame.ValidMoves(board, playerColor))
+                    {
+                        int score = ReversiGame.Score(ReversiGame.PlayTurn(board, p, playerColor), playerColor);
+                        if (score > bestScore)
+                        {
+                            bestMove = p;
+                            bestScore = score;
+                        }
+                    }
+                    ReversiGame.PlayTurn(board, bestMove, playerColor);
+                    foreach (Point p in ReversiGame.ValidMoves(board, playerColor))
+                    {
+                        int score = ReversiGame.Score(ReversiGame.PlayTurn(board, p, playerColor), playerColor);
+                        if (score > bestScore)
+                        {
+                            bestMove = p;
+                            bestScore = score;
+                        }
+                    }
+                    ReversiGame.PlayTurn(board, bestMove, playerColor);
+                    foreach (Point p in ReversiGame.ValidMoves(board, playerColor))
+                    {
+                        int score = ReversiGame.Score(ReversiGame.PlayTurn(board, p, playerColor), playerColor);
+                        if (score > bestScore)
+                        {
+                            bestMove = p;
+                            bestScore = score;
+                        }
+
+                    }
+                    ReversiGame.PlayTurn(board, bestMove, playerColor);
+                    foreach (Point p in ReversiGame.ValidMoves(board, playerColor))
+                    {
+                        int score = ReversiGame.Score(ReversiGame.PlayTurn(board, p, playerColor), playerColor);
+                        if (score > bestScore)
+                        {
+                            bestMove = p;
+                            bestScore = score;
+                        }
+
+                    }
+                    ReversiGame.PlayTurn(board, bestMove, playerColor);
+                    foreach (Point p in ReversiGame.ValidMoves(board, playerColor))
+                    {
+                        int score = ReversiGame.Score(ReversiGame.PlayTurn(board, p, playerColor), playerColor);
+                        if (score > bestScore)
+                        {
+                            bestMove = p;
+                            bestScore = score;
+                        }
+
+                    }
+                    ReversiGame.PlayTurn(board, bestMove, playerColor);
+                    foreach (Point p in ReversiGame.ValidMoves(board, playerColor))
+                    {
+                        int score = ReversiGame.Score(ReversiGame.PlayTurn(board, p, playerColor), playerColor);
+                        if (score > bestScore)
+                        {
+                            bestMove = p;
+                            bestScore = score;
+                        }
+
+                    }
                 }
             }
-            ReversiGame.PlayTurn(board, bestMove, playerColor);
-             foreach (Point p in ReversiGame.ValidMoves(board, playerColor))
-            {
-                int score = ReversiGame.Score(ReversiGame.PlayTurn(board, p, playerColor), playerColor);
-                if(score > bestScore)
-                {
-                    bestMove = p;
-                    bestScore = score;
-                }
-            }
-             ReversiGame.PlayTurn(board, bestMove, playerColor);
-             foreach (Point p in ReversiGame.ValidMoves(board, playerColor))
-             {
-                 int score = ReversiGame.Score(ReversiGame.PlayTurn(board, p, playerColor), playerColor);
-                 if (score > bestScore)
-                 {
-                     bestMove = p;
-                     bestScore = score;
-                 }
-
-             }
-             ReversiGame.PlayTurn(board, bestMove, playerColor);
-             foreach (Point p in ReversiGame.ValidMoves(board, playerColor))
-             {
-                 int score = ReversiGame.Score(ReversiGame.PlayTurn(board, p, playerColor), playerColor);
-                 if (score > bestScore)
-                 {
-                     bestMove = p;
-                     bestScore = score;
-                 }
-
-             }
-             ReversiGame.PlayTurn(board, bestMove, playerColor);
-             foreach (Point p in ReversiGame.ValidMoves(board, playerColor))
-             {
-                 int score = ReversiGame.Score(ReversiGame.PlayTurn(board, p, playerColor), playerColor);
-                 if (score > bestScore)
-                 {
-                     bestMove = p;
-                     bestScore = score;
-                 }
-
-             }
-             ReversiGame.PlayTurn(board, bestMove, playerColor);
-             foreach (Point p in ReversiGame.ValidMoves(board, playerColor))
-             {
-                 int score = ReversiGame.Score(ReversiGame.PlayTurn(board, p, playerColor), playerColor);
-                 if (score > bestScore)
-                 {
-                     bestMove = p;
-                     bestScore = score;
-                 }
-
-             }
             return bestMove;
         }
     }
